2817e6e717344a58c073e58ee1cbda0985665862
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-23 21:36:40 +0500

    TAsk 05

--- file05.py
+++ file05.py
@@ -1,10 +1,17 @@
 def main(data:str):
     """
-    The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
+    The data is from the file. Return the str(non-digital) characters as a list type.
     Args:
         data: str
     Returns:
         list: return answer
     """
     
+    a = []
+    idx = 0
+    while idx < len(data):
+        if not data[idx].isdigit():
+            a.append(data[idx])
+        idx += 1    
+    return a
 # Read data from file
