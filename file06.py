07de478a9ff2c98d07980a63f54ff81ba898a2a7
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-23 21:48:40 +0500

    TAsk 06

--- file06.py
+++ file06.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    x=data.split()
+    a=[]
+    idx=0
+    while idx < len(x):
+        print(data[idx])
+        a.append(len(x[idx]))
+        idx+1
+    return a
+
+    
+
     
 # Read data from file
