8284bb806f50bb171efe458fdf2abf738ecad974
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-23 22:13:23 +0500

    TAsk 10

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    list1 = data.split()
+    idx = 0
+    max = 0
+    while idx < len(list1):
+        if max < len(list1[idx]):
+            max = len(list1[idx])
+        idx += 1
+        
+    return max
+        
 
 # Read data from file
